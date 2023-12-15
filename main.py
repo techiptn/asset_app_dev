019dc1f7489ff303291324ee3f303e9137176404
Sung <techiptn@gmail.com>
2023-12-14 22:28:51 -0500

    ip 0,0,0,0

--- main.py
+++ main.py
@@ -287,4 +287,5 @@ def valid_only():
     return send_file(path, as_attachment=True, download_name= rename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=os.environ.get('DEBUG') == '1')
